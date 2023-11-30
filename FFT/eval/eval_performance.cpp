6cebd5c82b47a9152242a34829601456a6b76dab
Xemin0 <shilai_yang@brown.edu>
2023-11-29 19:00:04 -0700

    start working on eval_performance

--- FFT/eval/eval_performance.cpp
+++ FFT/eval/eval_performance.cpp
@@ -25,12 +25,16 @@ using namespace std;
 typedef complex<double> Complex;
 
 // Define the function pointer
-typedef void (*FuncPtr)(Complex*, int);
+typedef void (*FuncPtr)(Complex*, int);     // for 1D FFT methods
+typedef void (*FuncPtr2)(Complex**, int, int);   // for 2D FFT methods
+
+
+// ********** 1D FFT Performance Evaluation *********** //
 
 unsigned long time_FFT1d_4Data(Dataset1D& ds, FuncPtr func)
 {
 	/*
-	 * Time a single run of provided FFT method over the whole dataset
+	 * Time a single run of provided 1D FFT method over the whole dataset
 	 */
 	int rows, cols, depth;   
     ds.getDimensions(rows, cols, depth);
@@ -47,7 +51,7 @@ unsigned long time_FFT1d_4Data(Dataset1D& ds, FuncPtr func)
 	unsigned long tot_time = 0, start, end;
 
 	Complex tmp[truncated_cols];
-    // FFT for each channel/column
+    // 1D FFT for each channel/column
     for (int i = 0; i < rows; i++) 
         for (int k = 0; k < depth; k++){ 
             // load channel/row data to tmp
@@ -55,7 +59,7 @@ unsigned long time_FFT1d_4Data(Dataset1D& ds, FuncPtr func)
                 tmp[j] = ds.getElement(i+1, j+1, k+1); // Copy by value ?? not by reference??
             }   
 
-            // FFT with provided method (as a function pointer)
+            // 1D FFT with provided method (as a function pointer)
 			start = get_time();
             func(tmp, truncated_cols);
 			end = get_time();
@@ -71,7 +75,7 @@ unsigned long eval_FFT1d_4Data(Dataset1D& ds, FuncPtr func,
 					  bool toFile, std::string filename)
 {
  	/*
-     * Average Performance of FFT for a given Dataset in microsecond (us)
+     * Average Performance of 1D FFT for a given Dataset in microsecond (us)
 	 * (MAYBE) FLOPrate ** to be completed ** 
      *
 	 * Warmup runs are excluded in evaluation
@@ -112,3 +116,94 @@ unsigned long eval_FFT1d_4Data(Dataset1D& ds, FuncPtr func,
     timeFile.close();
 	return avg_t;
 }
+
+
+// ********** 2D FFT Performance Evaluation *********** //
+
+unsigned long time_FFT2d_4Data(Dataset2D& ds, FuncPtr2 func)
+{
+    /*  
+     * Time a single run of provided 2D FFT method over the whole dataset
+     */
+    int rows, cols, depth;   
+    ds.getDimensions(rows, cols, depth);
+    //cout << "rows = " << rows << endl;
+    //cout << "cols = " << cols << endl;
+    //cout << "depth = " << depth << endl;
+
+    // Iterative FFT requires input size to be a power of 2
+    int truncated_cols = log2(cols);
+    truncated_cols = static_cast<int>(pow(2, truncated_cols));
+    //cout << "resized column size: \t" << truncated_cols << endl;
+
+
+    unsigned long tot_time = 0, start, end;
+
+    Complex tmp[truncated_cols];
+    // FFT for each channel/column
+    for (int i = 0; i < rows; i++) 
+        for (int k = 0; k < depth; k++){ 
+            // load channel/row data to tmp
+            for (int j = 0; j < truncated_cols; j++){
+                tmp[j] = ds.getElement(i+1, j+1, k+1); // Copy by value ?? not by reference??
+            }   
+
+            // FFT with provided method (as a function pointer)
+            start = get_time();
+            func(tmp, truncated_cols);
+            end = get_time();
+
+            // Aggregate the timed result
+            tot_time += end - start;
+        }   
+    return tot_time; 
+}
+
+
+unsigned long eval_FFT2d_4Data(Dataset2D& ds, FuncPtr2 func, 
+                      int warmup, int testruns,
+                      bool toFile, std::string filename)
+{
+    /*
+     * Average Performance of 2D FFT for a given Dataset in microsecond (us)
+     * (MAYBE) FLOPrate ** to be completed ** 
+     *
+     * Warmup runs are excluded in evaluation
+     * 
+     * - FuncPtr2: Function pointer of 2D FFT method
+     *            in the form of `void func(Complex **x, int rows, int cols)` 
+     */
+
+    std::string root_path = "./Data/Results/";
+
+    // File name preparation
+    string time_filename = root_path + filename + "_us.dat";
+
+    
+    ofstream timeFile(time_filename); 
+    if (!timeFile.is_open()){
+        cerr << "Failed to open file for writing!" << endl;
+    }
+    cout << "Writing average time to: " << time_filename << endl;
+
+    unsigned long avg_t = 0;
+
+    // Warm up runs
+    for (int i = 0; i < warmup; i++)
+        time_FFT1d_4Data(ds, func);
+
+
+    // Recording times and take the average
+    for (int i = 0; i < testruns; i++)
+        avg_t += time_FFT1d_4Data(ds, func);
+
+    avg_t /= testruns;
+
+    // write to the file if specified
+    if (toFile)      
+        timeFile << "Average time of " << testruns << " runs: " << avg_t << " us" << endl;
+
+    timeFile.close();
+    return avg_t;
+}
+
