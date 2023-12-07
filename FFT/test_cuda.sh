4e5461e1e197c81a75215cff61a97907a1854c1f
Xemin0 <shilai_yang@brown.edu>
2023-12-07 13:34:41 -0700

    added SLURM script

---
+++ FFT/test_cuda.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Job name
+#SBATCH -J FFT_CUDA
+
+# Request a GPU node and 1 GPU
+#SBATCH -p gpu --gres=gpu:1
+
+# Ensures all allocated cores are on the same node
+#SBATCH -N 1
+
+# Request 1 CPU core
+#SBATCH -n 1
+
+# Request Memory
+#SBATCH --mem=20G
+
+#SBATCH -t 00:30:00
+
+#SBATCH -e ./Data/Oscar/job-%J.err
+#SBATCH -o ./Data/Oscar/job-%J.out
+
+# ==== End of SBATCH settings ==== #
+# Check GPU info
+nvidia-smi
+
+# Load CUDA and gcc
+module load cuda/11.2.0 gcc/10.2
+
+# Compile
+make all
+
+# Run and Profile
+#nsys profile --stats=true --force-overwrite=true -o report1 ./It_CT.out
+./It_CT.out
